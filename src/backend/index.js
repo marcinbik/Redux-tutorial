0b26197e2ccf8f7e39d3dbf6cb25adde21148514
marcinbik <marcinbik55@gmail.com>
2020-08-04 23:29:24 +0200

    add backend files

---
+++ src/backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const socketIO = require("socket.io");
+const http = require("http");
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on("connect", (socket) => {
+  console.log("We heve a new connection!");
+
+  
+
+   
+});
+
+
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
